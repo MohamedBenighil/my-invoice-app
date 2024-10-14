a60a028dd67a868c6100d6743d387ff1917731e8
Mohamed B. <mohamed.benighil07@gmail.com>
2024-10-14 11:55:55 +0200

    01.home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
+    // use h-screen instead of h-full, because h-full is relative and works only with parent where the hight is defined
     <main className="flex flex-col justify-center h-screen text-center max-w-5xl mx-auto gap-6">
       <h1 className="text-5xl font-bold">Invoicipedia</h1>
       <p>
