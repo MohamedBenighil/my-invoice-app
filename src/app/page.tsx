5d05cb4f88a721d4bea2081abf855b614db55805
Mohamed B. <mohamed.benighil07@gmail.com>
2024-10-14 11:53:26 +0200

    01.home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center h-full  text-center max-w-5xl mx-auto gap-6">
+    <main className="flex flex-col justify-center h-screen text-center max-w-5xl mx-auto gap-6">
       <h1 className="text-5xl font-bold">Invoicipedia</h1>
       <p>
         <Button asChild>
